9fd7de53f512baf6922ccea9df4a692edb6651d7
Lucaswil19 <lucas.wilbert@unifebe.edu.br>
2024-12-05 22:38:14 -0300

    Toast login e redefinir senha

--- assets/js/redefinirSenha.js
+++ assets/js/redefinirSenha.js
@@ -1,7 +1,7 @@
  // Função de validação do login
  window.redefineSenha = function redefineSenha() {
 
-alert("Senha nova verificada");
+toast("success","Senha nova verificada");
     
     const usuarioInput = document.getElementById("usuario");
     const senhaInput = document.getElementById("senha");
@@ -10,32 +10,61 @@ alert("Senha nova verificada");
     const redefinirSenhaUrl = `https://vendas-comercialize-a0fqhjhne5cagkc5.brazilsouth-01.azurewebsites.net/Usuario/atualizar-senha-usuario/${usuario}/${senhaNova}`;
     
     fetch(redefinirSenhaUrl, {
-        method: 'PUT' // Altere para POST se necessário
+        method: 'PUT' 
     })
     .then(response => {
         if (response.ok) {
-            window.location.href = "login.html"; // Redireciona para index.html
+            window.location.href = "login.html"; 
         } else {
-            alert("Usuário não encontrado");
-            usuarioInput.value = ""; // Limpa o campo de usuário
-            senhaInput.value = ""; // Limpa o campo de senha
-            usuarioInput.focus(); // Foca no campo de usuário
+            toast('error',"Usuário não encontrado");
+            usuarioInput.value = ""; 
+            senhaInput.value = ""; 
+            usuarioInput.focus(); 
         }
        
     })
     .catch(error => {
         console.error("Erro:", error);
-        alert("Ocorreu um erro ao tentar mudar sua senha, verifique se o nome de usuário está correto e tente novamente.");
+        toast('error',"Ocorreu um erro ao tentar mudar sua senha, verifique se o nome de usuário está correto e tente novamente.");
     });
 
 }
 
-// Adiciona o evento de pressionar "Enter" ao campo de senha
+
 document.addEventListener("DOMContentLoaded", function () {
     const senhaInput = document.getElementById("senha");
     senhaInput.addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
-            redefineSenha(); // Chama a função redefineSenha() ao pressionar Enter
+            redefineSenha(); 
         }
     });
 });
+
+/===== TOAST  =====/
+function toast(tipoToast, mensagem) {
+  switch (tipoToast) {
+    case "success":
+
+      Toastify({
+        text: mensagem,
+        className: "success",
+        style: {
+          background: "linear-gradient(to right,  #711e92, #5b087c)",
+        }
+      }).showToast();
+
+      break;
+    case "error":
+
+      Toastify({
+        text: mensagem,
+        className: "error",
+        style: {
+          background: "linear-gradient(to right, #ff0000, #b30000, #800000)"
+        }
+      }).showToast();
+
+      break;
+  }
+
+}
